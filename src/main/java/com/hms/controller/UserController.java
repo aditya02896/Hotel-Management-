e06020e3fcb09922da26c3006286c4413625cb41
Aditya maurya <adityacode49@gmail.com>
2025-04-04 11:21:49 +0530

    add file

--- src/main/java/com/hms/controller/UserController.java
+++ src/main/java/com/hms/controller/UserController.java
@@ -3,7 +3,6 @@ import com.hms.entity.AppUser;
 import com.hms.payload.LoginDto;
 import com.hms.repository.AppUserRepository;
 import com.hms.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
